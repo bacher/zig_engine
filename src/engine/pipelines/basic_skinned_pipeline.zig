3363ee562e7174afbba6c92da128a4d440019677
Ilia Lebedev <bacherko@gmail.com>
2026-06-26 10:46:59 +0200

    enable normal capturing for skeletal pipeline

--- src/engine/pipelines/basic_skinned_pipeline.zig
+++ src/engine/pipelines/basic_skinned_pipeline.zig
@@ -6,7 +6,7 @@ const wgsl_fs = @embedFile("../shaders/basic/fs.wgsl");
 
 const Pipeline = @import("../pipeline.zig").Pipeline;
 const BindGroupLayouts = @import("../bind_group_layouts.zig").BindGroupLayouts;
-const first_pass_color_targets = @import("./_first_pass_color_targets.zig").first_pass_color_targets;
+const first_pass_color_with_normals_targets = @import("./_first_pass_color_targets.zig").first_pass_color_with_normals_targets;
 
 pub fn createBasicSkinnedPipeline(
     gctx: *zgpu.GraphicsContext,
@@ -81,8 +81,8 @@ pub fn createBasicSkinnedPipeline(
         .fragment = &wgpu.FragmentState{
             .module = fs_module,
             .entry_point = "main",
-            .targets = &first_pass_color_targets,
-            .target_count = first_pass_color_targets.len,
+            .targets = &first_pass_color_with_normals_targets,
+            .target_count = first_pass_color_with_normals_targets.len,
         },
     };
 
