2da82a6b20be8f7f93ca63ad7663cc13ced43fed
Ilia Lebedev <bacherko@gmail.com>
2026-06-26 10:50:10 +0200

    use rgb10_a2_unorm for g-buffer normals

--- src/engine/pipelines/_first_pass_color_targets.zig
+++ src/engine/pipelines/_first_pass_color_targets.zig
@@ -2,7 +2,7 @@ const zgpu = @import("zgpu");
 const wgpu = zgpu.wgpu;
 
 pub const COLOR_OUTPUT_FORMAT = wgpu.TextureFormat.rgba8_unorm;
-pub const NORMAL_OUTPUT_FORMAT = wgpu.TextureFormat.rgba8_unorm;
+pub const NORMAL_OUTPUT_FORMAT = wgpu.TextureFormat.rgb10_a2_unorm;
 
 // TODO: Why I can't use `wgpu.TextureFormat.rgba8_snorm` for normals?
 // error: [zgpu] Validation: The texture usage (TextureUsage::(TextureBinding|RenderAttachment)) includes TextureUsage::RenderAttachment, which is incompatible with the non-renderable format (TextureFormat::RGBA8Snorm).
